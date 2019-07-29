d18dcffafe9a310677d93f43377ef3565b027e47
jiji <jijeong0114@gmail.com>
2019-07-29 20:42:43 +0900

    Updates

---
+++ precache-manifest.e41226cd82d99e6b0c841ffdbb6cb9d2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1eff373dfe9bfc5be43d51f5e538437",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3a7fe09ea6e72294aca6",
+    "url": "/portfolio/static/css/main.22850019.chunk.css"
+  },
+  {
+    "revision": "435163dd44e14965feb8",
+    "url": "/portfolio/static/js/2.716d88e2.chunk.js"
+  },
+  {
+    "revision": "3a7fe09ea6e72294aca6",
+    "url": "/portfolio/static/js/main.ba403f5f.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "f71e1f583926954ea6fd9c07385f4cea",
+    "url": "/portfolio/static/media/airApp1.f71e1f58.JPG"
+  },
+  {
+    "revision": "044d1e1aff9397398b0ce66b787d7279",
+    "url": "/portfolio/static/media/getAQI1.044d1e1a.jpg"
+  },
+  {
+    "revision": "16626cf9943d5725c020aa8773572125",
+    "url": "/portfolio/static/media/pms1.16626cf9.PNG"
+  },
+  {
+    "revision": "a1ae78d56366447eefc1edfdef110f44",
+    "url": "/portfolio/static/media/profile.a1ae78d5.jpg"
+  }
+]);
