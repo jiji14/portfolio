22d7131f5e8d2ad5a111f7a2b02f8e802da0433b
jiji <jijeong0114@gmail.com>
2019-07-29 13:42:50 +0900

    add images

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -27,7 +27,7 @@ function Resume() {
                         <p className="term">2019.3 ~ 2019.8</p>
                     </div>
                     <div className="detailBox">
-                        <p>서울여자대학교 의류학과 (3.79/4.5)</p>
+                        <p>서울여자대학교 의류학과 (3.79 / 4.5)</p>
                         <p className="term">2010.3 ~ 2015.8</p>
                     </div>
                 </div>
@@ -39,11 +39,11 @@ function Resume() {
                 </div>
                 <div className="details">
                     <div className="detailBox">
-                        <p >서울여자대학교 재무팀  -  출납업무 및 재무관리</p>
+                        <p >서울여자대학교 재무팀 (출납업무 및 재무관리)</p>
                         <p className="term">2016.11 ~ 2018.11</p>
                     </div>
                     <div className="detailBox">
-                        <p>신한은행 인천공항점  -  환전 및 입출금업무</p>
+                        <p>신한은행 인천공항점 (환전 및 입출금업무)</p>
                         <p className="term">2015.9 ~ 2016.10</p>
                     </div>
                 </div>
--- src/components/Works.js
+++ src/components/Works.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import project1 from '../image/project1.PNG';
+import project1 from '../image/pms1.PNG';
+import airApp1 from '../image/airApp1.JPG';
+import getAQI1 from '../image/getAQI1.JPG';
 import '../css/Works.css';
 
 function Works() {
@@ -10,13 +12,13 @@ function Works() {
             WORKS
         </div>
         <div className="workWrap">
-            <div>
-                <img src={project1} className="workImg"/>
+            <div className="items">
+                <img src={getAQI1} className="workImg"/>
             </div>
-            <div>
-                <img src={project1} className="workImg"/>
+            <div className="items">
+                <img src={airApp1} className="workImg"/>
             </div>
-            <div>
+            <div className="items">
                 <img src={project1} className="workImg"/>
             </div>
         </div>   
