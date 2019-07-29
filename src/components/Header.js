5385f3cafb35cf57238273ba168622b2de1cb025
jiji <jijeong0114@gmail.com>
2019-07-29 13:53:13 +0900

    header goDown img add

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import '../css/Header.css';
+import down from '../image/down.png';
 
 function Header() {
 
   return(
-    <div className="headerContainer">
-      <div className="headerList">
-        <ul>
-            <li>HOME</li>
-            <li>ABOUT</li>
-            <li>RESUME</li>
-            <li>WORKS</li>
-            <li>CONTACT</li>
-        </ul>
+    <div>
+      <div className="headerContainer">   
+        <div className="headerList">
+          <ul>
+              <li>HOME</li>
+              <li>ABOUT</li>
+              <li>RESUME</li>
+              <li>WORKS</li>
+              <li>CONTACT</li>
+          </ul>
+        </div>
+        
+      </div>
+      <div className="goDown">
+        <img src={down} className="downImg" />
       </div>
     </div>
   )
