827d42437e1353079d7ce044aaa6b35ba1e3145c
jiji <jijeong0114@gmail.com>
2019-07-29 20:31:38 +0900

    Add animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,10 +6,13 @@ function Home() {
 
   return(
     <div className="home" id="home">
+        <div className="mainText">
+            Hello! Welcome to my world :)
+        </div>
         <div className="goDown" >
-        <a href="#contact">
-          <img src={down} className="downImg" />
-        </a>
+            <a href="#contact">
+            <img src={down} className="downImg" />
+            </a>
         </div>
     </div>
   )
