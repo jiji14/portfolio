94a69b868788b3ca9bb6f3cd91f556f1d89cedd2
jiji <jijeong0114@gmail.com>
2019-07-30 13:27:50 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.e41226cd82d99e6b0c841ffdbb6cb9d2.js"
+  "/portfolio/precache-manifest.5e610f4d2305d1a9021c40f11e76ffa0.js"
 );
 
 self.addEventListener('message', (event) => {
