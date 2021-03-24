9ca30e5adbfdb301138377cdd70d0f9e120cab13
jijeong <jijeong0114@gmail.com>
2021-03-24 22:33:04 +0900

    소개글 소개 및 인포 아이콘 추가

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import profile from '../image/profile.jpg';
 import '../css/About.css';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import EmailIcon from '@material-ui/icons/Email';
 
 function About() {
 
@@ -13,25 +16,31 @@ function About() {
             ABOUT
         </div>
         <div className="infoWrap">
-            <div className="characters">
+            {/* <div className="characters">
               { personality.map((p, i) => {
                 return (
                   <div key={p+i}>{p}</div>
                 )
               })}
-            </div>
+            </div> */}
             <div className="profileBox">
               <img src={profile} className="profile" alt=""/>
               <div className="aboutMe">
                   <div className="introduce">
-                    <p className="info">WHO ARE YOU?</p>
-                    <p className="desc">배우는 것을 좋아하며 코딩을 사랑하는 열정 가득한 신입 프론트엔드 개발자 이지정입니다. </p>
+                    {/* <p className="info">ABOUT ME</p> */}
+                    <p className="desc">2년차 주니어 프론트엔드 개발자 이지정입니다. </p>
+                    <p className="desc">배움에 있어 열정적이며, 소통에 강한 팀플레이어 입니다.</p>
                   </div>
                   <div className="contactBox">
-                    <p className="info">CONTACT INFO</p>
-                    <p className="desc">Email : jijeong0114@gmail.com</p>
-                    <p className="desc">Github : <a href="https://github.com/jiji14">https://github.com/jiji14</a></p>
-                    <p className="desc">LinkedIn : <a href="https://www.linkedin.com/in/JijeongLee">https://www.linkedin.com/in/JijeongLee</a></p>
+                    {/* <p className="info">CONTACT INFO</p> */}
+                    <p className="desc flex">
+                      <EmailIcon className="mail" fontSize="large" />
+                      jijeong0114@gmail.com
+                      </p>
+                    <p className="desc">
+                      <GitHubIcon className="icon" onClick={()=>{window.open("https://github.com/jiji14", "_blank");}} />
+                      <LinkedInIcon className="icon" onClick={()=>{window.open("https://www.linkedin.com/in/JijeongLee", "_blank");}}/>
+                    </p>
                   </div>
               </div>    
             </div>
