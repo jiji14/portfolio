9e7ce45bb4f422afebb13bb1c73d1f8835e08ab5
jijeong <jijeong0114@gmail.com>
2021-03-28 01:38:34 +0900

    React JS -> React

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -17,11 +17,10 @@ function Resume() {
 
     // 이력사항 배열에 저장후 반복문 돌리기 
     const education = [{course: 'KG ITBANK 자바기반 웹개발자 과정', term: '2019.3 ~ 2019.8'}, {course: '서울여자대학교 의류학과', term: '2010.3 ~ 2015.8'}]
-    const work = [{course: '디에스랩 글로벌 개발팀', detail: ['- 웹서비스 프론트엔트 개발 (React/Redux)', '- 서비스 기획 및 UI/UX 개선'], term: '2019.8 ~ 재직중'}]
+    const work = [{course: '디에스랩 글로벌 개발팀', detail: ['- 웹서비스 프론트엔트 개발', '- 서비스 기획 및 UI/UX 개선'], term: '2019.8 ~ 재직중'}]
     const skills = [{name: 'HTML', src: html}, {name: 'CSS', src: css}, {name: 'Javascript', src: javascript}, 
-    {name : 'Typescript', src: typescript}, {name: 'React/React Native', src: react}, {name: 'Redux/Saga', src: redux},
-    {name: 'Firebase', src: firebase}, {name: 'Git', src: git},
-    {name: 'Jest', src: jest}, {name: 'Cypress', src: cypress, isBig: true}];
+    {name : 'Typescript', src: typescript}, {name: 'React /React Native', src: react}, {name: 'Redux/Saga', src: redux},
+    {name: 'Firebase', src: firebase}, {name: 'Jest', src: jest}, {name: 'Cypress', src: cypress, isBig: true}, {name: 'Git', src: git}];
 
     return(
     <div className="ResumeContainer" id="resume">
