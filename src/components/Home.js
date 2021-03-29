b294b0e7bf61cffb52c4878ffa9ae56c98543921
jijeong <jijeong0114@gmail.com>
2021-03-29 22:49:20 +0900

    홈 문구 추가

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import '../css/Home.css';
 import down from '../image/doubleDown.png';
 
 function Home() {
 
+  const [index, setIndex] = useState(0);
+  const [isTextChanged, setIsTextChanged] = useState(true);
+  const texts = ["Jijeong Lee, Front-End Developer", "Webapps, Mobile apps", "React Javascript Typescript Firebase"];
+  
+  useEffect(()=>{
+    setTimeout(()=>{
+      setIndex((index+1) % 3);
+      setIsTextChanged(false);
+    }, [4000])
+  }, [index])
+
+  useEffect(()=>{
+    if(!isTextChanged) setIsTextChanged(true);
+  }, [isTextChanged])
+
   return(
     <div className="home" id="home">
+        {
+        isTextChanged &&
         <div className="mainText">
-          Jijeong Lee, Front-End Developer
+          {texts[index]}
         </div>
+        }
         <div className="goDown" >
             <a href="#footer">
               <img src={down} className="downImg" alt="" />
