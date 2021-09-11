0a2f13aeedbe0c87d48ff529933aa10e6493747c
jijeong <jijeong0114@gmail.com>
2021-09-11 12:23:56 +0900

    스택 - nextjs 추가 및 경력 추가

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -11,15 +11,15 @@ import firebase from '../image/stack/firebase.png';
 import git from '../image/stack/git.png';
 import jest from '../image/stack/jest.png';
 import cypress from '../image/stack/cypress.png';
-import Tooltip from '@material-ui/core/Tooltip';
+import nextjs from '../image/stack/nextjs.png';
 
 function Resume() {
 
     // 이력사항 배열에 저장후 반복문
     const education = [{course: 'KG ITBANK 자바기반 웹개발자 과정', term: '2019.3 ~ 2019.8'}, {course: '서울여자대학교 의류학과', term: '2010.3 ~ 2015.8'}]
-    const work = [{course: '디에스랩 글로벌 개발팀', detail: ['- 웹서비스 프론트엔트 개발', '- 서비스 기획 및 UI/UX 개선'], term: '2019.8 ~ 2021.4'}]
+    const work = [{course: '놀이의발견 플랫폼실', detail: ['- 웹서비스 프론트엔트 개발'], term: '2021.9 ~ 재직중'}, {course: '디에스랩 글로벌 개발팀', detail: ['- 웹서비스 프론트엔트 개발', '- 서비스 기획 및 UI/UX 개선'], term: '2019.8 ~ 2021.4'}]
     const skills = [{name: 'HTML', src: html}, {name: 'CSS', src: css}, {name: 'Javascript', src: javascript}, 
-    {name : 'Typescript', src: typescript}, {name: 'React /React Native', src: react}, {name: 'Redux/Saga', src: redux},
+    {name : 'Typescript', src: typescript}, {name: 'React /React Native', src: react}, {name: 'Next.js', src: nextjs}, {name: 'Redux/Saga', src: redux},
     {name: 'Firebase', src: firebase}, {name: 'Jest', src: jest}, {name: 'Cypress', src: cypress, isBig: true}, {name: 'Git', src: git}];
 
     return(
