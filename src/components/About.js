4a6a2e921a20821095d0d038056deaa83342eb97
jijeong <jijeong0114@gmail.com>
2021-12-05 13:06:51 +0900

    i18n 적용

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import profile from '../image/profile.jpg';
 import '../css/About.css';
 import GitHubIcon from '@material-ui/icons/GitHub';
@@ -7,8 +8,7 @@ import EmailIcon from '@material-ui/icons/Email';
 
 function About() {
 
-  // 본인을 표현할 수 있는 성격 배열에 저장후 반복문 돌리기 
-  const personality = ['Positive', 'HardWorking', 'FastLearning', 'Passionate'];
+  const { t }  = useTranslation(['page']);
 
   return(
     <div className="homeContainer" id="about">
@@ -16,20 +16,16 @@ function About() {
             ABOUT
         </div>
         <div className="infoWrap">
-            {/* <div className="characters">
-              { personality.map((p, i) => {
-                return (
-                  <div key={p+i}>{p}</div>
-                )
-              })}
-            </div> */}
             <div className="profileBox">
               <img src={profile} className="profile" alt=""/>
               <div className="aboutMe">
                   <div className="introduce">
                     {/* <p className="info">ABOUT ME</p> */}
-                    <p className="desc">2년차  프론트엔드 개발자 이지정입니다. </p>
-                    <p className="desc">배움에 있어 열정적이며, 소통에 강한 팀플레이어 입니다.</p>
+                    <p className="desc">
+                      {t('2년차 프론트엔드 개발자 이지정입니다.')}
+                      {' '}
+                      {t('배움에 있어 열정적이며, 소통에 강한 팀플레이어 입니다.')}
+                    </p>
                   </div>
                   <div className="contactBox">
                     {/* <p className="info">CONTACT INFO</p> */}
--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import '../css/Detail.css';
 
 function Detail({title, details}){
 
     const _title = title.toUpperCase();
+    const { t }  = useTranslation(['page']);
 
     return(
         <div className={title}>
@@ -15,11 +17,11 @@ function Detail({title, details}){
                 {details.map((detail, i) => {
                     return(
                         <div className="detailBox" key={detail+i}>
-                            <p >{detail.course}</p>
+                            <p >{t(detail.course)}</p>
                             <ul>{detail.detail && detail.detail.map((item)=>{
-                                return <li className="item">{item}</li>
+                                return <li className="item">{t(item)}</li>
                             })}</ul>
-                            <p className="term">{detail.term}</p>
+                            <p className="term">{t(detail.term)}</p>
                         </div>
                     )
                 })}
